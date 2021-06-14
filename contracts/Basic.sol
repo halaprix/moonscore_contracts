0b1627080d5d07d4371cf103a57f5b9ee883b8d9
paniczklos <panicz.klos@gmail.com>
2021-06-14 21:36:28 +0200

    initial

---
+++ contracts/Basic.sol
@@ -0,0 +1,455 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+import "./interfaces/IPancakeswapFarm.sol";
+
+import "./interfaces/IPancakeRouter01.sol";
+
+import "./interfaces/IPancakeRouter02.sol";
+
+interface IWBNB is IERC20 {
+    function deposit() external payable;
+
+    function withdraw(uint256 wad) external;
+}
+
+contract Basic is Ownable, ReentrancyGuard, Pausable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    bool public isCAKEStaking; // pid = 0 in pancake masterschefs
+    bool public isSameAssetDeposit; // false - FARM / true - POOL
+    bool public isAutoComp; // false - FARM / true - VAULT
+
+    address public farmContractAddress; // masterChef
+    address public PIXELAddress;
+    uint256 public pid; // pid of the farm in farmContractAddress
+    address public wantAddress; // address of the staked token (LP)
+    address public token0Address; // token 0 of want
+    address public token1Address; // token 1 of want
+    address public earnedAddress; // masterChef reward
+    address public uniRouterAddress; // router address
+
+    address public constant wbnbAddress =
+        0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
+    address public autoFarmAddress; // PIXEL farm address
+    address public govAddress; // timelock contract
+    address public feeAddress; // timelock contract
+    bool public onlyGov = false;
+
+    uint256 public buyBackRate = 0; // 0%
+    uint256 public constant buyBackRateDenominator = 10000; // 100 = 1%
+    uint256 public constant buyBackRateUL = 800;
+    address public buyBackAddress = 0x000000000000000000000000000000000000dEaD;
+
+    uint256 public lastEarnBlock = 0;
+    uint256 public wantLockedTotal = 0;
+    uint256 public sharesTotal = 0;
+
+    uint256 public controllerFee = 1500;
+    uint256 public constant controllerFeeDenominator = 10000;
+    uint256 public constant controllerFeeUL = 3000;
+
+    uint256 public entranceFeeFactor = 9990; // < 0.1% entrance fee - goes to pool + prevents front-running
+    uint256 public constant entranceFeeFactorDenominator = 10000;
+    uint256 public constant entranceFeeFactorLL = 9950; // 0.5% is the max entrance fee settable.
+
+    uint256 public slippageFactor = 950; // 5% default slippage tolerance
+    uint256 public constant slippageFactorUL = 995;
+
+    address[] public earnedToToken0Path;
+    address[] public earnedToToken1Path;
+    address[] public token0ToEarnedPath;
+    address[] public token1ToEarnedPath;
+    address[] public earnedToPIXELPath;
+
+    constructor(
+        address _autoFarmAddress,
+        address _feeAddress,
+        bool _isCAKEStaking,
+        bool _isSameAssetDeposit,
+        bool _isAutoComp,
+        address _farmContractAddress,
+        uint256 _pid,
+        address _wantAddress,
+        address _token0Address,
+        address _token1Address,
+        address _earnedAddress,
+        address _uniRouterAddress
+    ) {
+        govAddress = msg.sender;
+        feeAddress = _feeAddress;
+        autoFarmAddress = _autoFarmAddress;
+
+        isCAKEStaking = _isCAKEStaking;
+        isSameAssetDeposit = _isSameAssetDeposit;
+        isAutoComp = _isAutoComp;
+        wantAddress = _wantAddress;
+
+        if (isAutoComp) {
+            if (!isCAKEStaking && !isSameAssetDeposit) {
+                token0Address = _token0Address;
+                token1Address = _token1Address;
+            }
+
+            farmContractAddress = _farmContractAddress;
+            pid = _pid;
+            earnedAddress = _earnedAddress;
+
+            uniRouterAddress = _uniRouterAddress;
+
+            earnedToToken0Path = [earnedAddress, wbnbAddress, token0Address];
+            if (wbnbAddress == token0Address) {
+                earnedToToken0Path = [earnedAddress, wbnbAddress];
+            }
+
+            earnedToToken1Path = [earnedAddress, wbnbAddress, token1Address];
+            if (wbnbAddress == token1Address) {
+                earnedToToken1Path = [earnedAddress, wbnbAddress];
+            }
+
+            token0ToEarnedPath = [token0Address, wbnbAddress, earnedAddress];
+            if (wbnbAddress == token0Address) {
+                token0ToEarnedPath = [wbnbAddress, earnedAddress];
+            }
+
+            token1ToEarnedPath = [token1Address, wbnbAddress, earnedAddress];
+            if (wbnbAddress == token1Address) {
+                token1ToEarnedPath = [wbnbAddress, earnedAddress];
+            }
+
+            earnedToPIXELPath = [earnedAddress, wbnbAddress, PIXELAddress];
+        }
+        transferOwnership(autoFarmAddress);
+    }
+
+    function deposit(address _userAddress, uint256 _wantAmt)
+        public
+        onlyOwner
+        whenNotPaused
+        returns (uint256)
+    {
+        IERC20(wantAddress).safeTransferFrom(
+            address(msg.sender),
+            address(this),
+            _wantAmt
+        );
+
+        uint256 sharesAdded = _wantAmt;
+        if (wantLockedTotal > 0) {
+            sharesAdded = _wantAmt
+                .mul(sharesTotal)
+                .mul(entranceFeeFactor)
+                .div(wantLockedTotal)
+                .div(entranceFeeFactorDenominator);
+        }
+        sharesTotal = sharesTotal.add(sharesAdded);
+
+        if (isAutoComp) {
+            _farm();
+        } else {
+            wantLockedTotal = wantLockedTotal.add(_wantAmt);
+        }
+
+        return sharesAdded;
+    }
+
+    function farm() public nonReentrant {
+        _farm();
+    }
+
+    function _farm() internal {
+        uint256 wantAmt = IERC20(wantAddress).balanceOf(address(this));
+        wantLockedTotal = wantLockedTotal.add(wantAmt);
+        IERC20(wantAddress).safeIncreaseAllowance(farmContractAddress, wantAmt);
+
+        if (isCAKEStaking) {
+            IPancakeswapFarm(farmContractAddress).enterStaking(wantAmt); // Just for CAKE staking, we dont use deposit()
+        } else {
+            IPancakeswapFarm(farmContractAddress).deposit(pid, wantAmt);
+        }
+    }
+
+    function withdraw(address _userAddress, uint256 _wantAmt)
+        public
+        onlyOwner
+        nonReentrant
+        returns (uint256)
+    {
+        require(_wantAmt > 0, "_wantAmt <= 0");
+
+        if (isAutoComp) {
+            if (isCAKEStaking) {
+                IPancakeswapFarm(farmContractAddress).leaveStaking(_wantAmt); // Just for CAKE staking, we dont use withdraw()
+            } else {
+                IPancakeswapFarm(farmContractAddress).withdraw(pid, _wantAmt);
+            }
+        }
+
+        uint256 wantAmt = IERC20(wantAddress).balanceOf(address(this));
+        if (_wantAmt > wantAmt) {
+            _wantAmt = wantAmt;
+        }
+
+        if (wantLockedTotal < _wantAmt) {
+            _wantAmt = wantLockedTotal;
+        }
+
+        uint256 sharesRemoved = _wantAmt.mul(sharesTotal).div(wantLockedTotal);
+        if (sharesRemoved > sharesTotal) {
+            sharesRemoved = sharesTotal;
+        }
+        sharesTotal = sharesTotal.sub(sharesRemoved);
+        wantLockedTotal = wantLockedTotal.sub(_wantAmt);
+
+        IERC20(wantAddress).safeTransfer(autoFarmAddress, _wantAmt);
+
+        return sharesRemoved;
+    }
+
+    function earn() public whenNotPaused {
+        require(isAutoComp, "!isAutoComp");
+        if (onlyGov) {
+            require(msg.sender == govAddress, "Not authorised");
+        }
+
+        // Harvest farm tokens
+        if (isCAKEStaking) {
+            IPancakeswapFarm(farmContractAddress).leaveStaking(0); // Just for CAKE staking, we dont use withdraw()
+        } else {
+            IPancakeswapFarm(farmContractAddress).withdraw(pid, 0);
+        }
+
+        // Converts farm tokens into want tokens
+        uint256 earnedAmt = IERC20(earnedAddress).balanceOf(address(this));
+
+        earnedAmt = distributeFees(earnedAmt);
+        if (buyBackRate > 0) {
+             earnedAmt = buyBack(earnedAmt);
+        }
+
+        if (isCAKEStaking || isSameAssetDeposit) {
+            lastEarnBlock = block.number;
+            _farm();
+            return;
+        }
+        IERC20(earnedAddress).safeIncreaseAllowance(
+            uniRouterAddress,
+            earnedAmt
+        );
+
+        if (earnedAddress != token0Address) {
+            // Swap half earned to token0
+            _safeSwap(
+                uniRouterAddress,
+                earnedAmt.div(2),
+                slippageFactor,
+                earnedToToken0Path,
+                address(this),
+                block.timestamp.add(600)
+            );
+        }
+
+        if (earnedAddress != token1Address) {
+            // Swap half earned to token0
+            _safeSwap(
+                uniRouterAddress,
+                earnedAmt.div(2),
+                slippageFactor,
+                earnedToToken1Path,
+                address(this),
+                block.timestamp.add(600)
+            );
+        }
+
+        // Get want tokens, ie. add liquidity
+        uint256 token0Amt = IERC20(token0Address).balanceOf(address(this));
+        uint256 token1Amt = IERC20(token1Address).balanceOf(address(this));
+        if (token0Amt > 0 && token1Amt > 0) {
+            IERC20(token0Address).safeIncreaseAllowance(
+                uniRouterAddress,
+                token0Amt
+            );
+            IERC20(token1Address).safeIncreaseAllowance(
+                uniRouterAddress,
+                token1Amt
+            );
+            IPancakeRouter02(uniRouterAddress).addLiquidity(
+                token0Address,
+                token1Address,
+                token0Amt,
+                token1Amt,
+                0,
+                0,
+                address(this),
+                block.timestamp.add(600)
+            );
+        }
+        lastEarnBlock = block.number;
+        _farm();
+    }
+
+    function distributeFees(uint256 _earnedAmt) internal returns (uint256) {
+        if (_earnedAmt > 0) {
+            // Performance fee
+            if (controllerFee > 0) {
+                uint256 fee =
+                    _earnedAmt.mul(controllerFee).div(controllerFeeDenominator);
+                IERC20(earnedAddress).safeTransfer(feeAddress, fee);
+                _earnedAmt = _earnedAmt.sub(fee);
+            }
+        }
+
+        return _earnedAmt;
+    }
+
+    function convertDustToEarned() public whenNotPaused {
+        require(isAutoComp, "!isAutoComp");
+        require(!isCAKEStaking && !isSameAssetDeposit, "Single pool strategy");
+
+        // Converts dust tokens into earned tokens, which will be reinvested on the next earn().
+
+        // Converts token0 dust (if any) to earned tokens
+        uint256 token0Amt = IERC20(token0Address).balanceOf(address(this));
+        if (token0Address != earnedAddress && token0Amt > 0) {
+            IERC20(token0Address).safeIncreaseAllowance(
+                uniRouterAddress,
+                token0Amt
+            );
+
+            // Swap all dust tokens to earned tokens
+            _safeSwap(
+                uniRouterAddress,
+                token0Amt,
+                slippageFactor,
+                token0ToEarnedPath,
+                address(this),
+                block.timestamp.add(600)
+            );
+        }
+
+        // Converts token1 dust (if any) to earned tokens
+        uint256 token1Amt = IERC20(token1Address).balanceOf(address(this));
+        if (token1Address != earnedAddress && token1Amt > 0) {
+            IERC20(token1Address).safeIncreaseAllowance(
+                uniRouterAddress,
+                token1Amt
+            );
+
+            // Swap all dust tokens to earned tokens
+            _safeSwap(
+                uniRouterAddress,
+                token1Amt,
+                slippageFactor,
+                token1ToEarnedPath,
+                address(this),
+                block.timestamp.add(600)
+            );
+        }
+    }
+
+    function pause() public {
+        require(msg.sender == govAddress, "Not authorised");
+        _pause();
+    }
+
+    function unpause() external {
+        require(msg.sender == govAddress, "Not authorised");
+        _unpause();
+    }
+
+    function setEntranceFeeFactor(uint256 _entranceFeeFactor) external {
+        require(msg.sender == govAddress, "Not authorised");
+        require(_entranceFeeFactor > entranceFeeFactorLL, "!safe - too low");
+        require(_entranceFeeFactor <= entranceFeeFactorDenominator, "!safe - too high");
+        entranceFeeFactor = _entranceFeeFactor;
+    }
+
+    function setControllerFee(uint256 _controllerFee) external {
+        require(msg.sender == govAddress, "Not authorised");
+        require(_controllerFee <= controllerFeeUL, "too high");
+        controllerFee = _controllerFee;
+    }
+
+    function setGov(address _govAddress) external {
+        require(msg.sender == govAddress, "!gov");
+        govAddress = _govAddress;
+    }
+
+    function setOnlyGov(bool _onlyGov) external {
+        require(msg.sender == govAddress, "!gov");
+        onlyGov = _onlyGov;
+    }
+
+    function inCaseTokensGetStuck(
+        address _token,
+        uint256 _amount,
+        address _to
+    ) external {
+        require(msg.sender == govAddress, "!gov");
+        require(_token != earnedAddress, "!safe");
+        require(_token != wantAddress, "!safe");
+        IERC20(_token).safeTransfer(_to, _amount);
+    }
+
+    function _safeSwap(
+        address _uniRouterAddress,
+        uint256 _amountIn,
+        uint256 _slippageFactor,
+        address[] memory _path,
+        address _to,
+        uint256 _deadline
+    ) internal virtual {
+        uint256[] memory amounts =
+            IPancakeRouter02(_uniRouterAddress).getAmountsOut(_amountIn, _path);
+        uint256 amountOut = amounts[amounts.length.sub(1)];
+
+        IPancakeRouter02(_uniRouterAddress)
+            .swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            _amountIn,
+            amountOut.mul(_slippageFactor).div(1000),
+            _path,
+            _to,
+            _deadline
+        );
+    }
+
+    function buyBack(uint256 _earnedAmt) internal virtual returns (uint256) {
+        if (buyBackRate <= 0) {
+            return _earnedAmt;
+        }
+
+        uint256 buyBackAmt =
+            _earnedAmt.mul(buyBackRate).div(buyBackRateDenominator);
+
+        if (earnedAddress == PIXELAddress) {
+            IERC20(earnedAddress).safeTransfer(buyBackAddress, buyBackAmt);
+        } else {
+            IERC20(earnedAddress).safeIncreaseAllowance(
+                uniRouterAddress,
+                buyBackAmt
+            );
+
+            _safeSwap(
+                uniRouterAddress,
+                buyBackAmt,
+                slippageFactor,
+                earnedToPIXELPath,
+                buyBackAddress,
+                block.timestamp.add(600)
+            );
+        }
+
+        return _earnedAmt.sub(buyBackAmt);
+    }
+}
---
+++ contracts/PIXEL.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+//import "./helpers/ERC20.sol";
+
+import "@openzeppelin/contracts/utils/Address.sol";
+//import "./libraries/Address.sol";
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+//import "./libraries/SafeERC20.sol";
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+//import "./helpers/Ownable.sol";
+
+contract PIXEL is ERC20("PIXELtest", "PXLtst"), Ownable {
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
+}
